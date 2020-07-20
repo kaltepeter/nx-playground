89c5bfa747c0d0228e43f903237b36c67c51cee5
Kayla Altepeter <kayla@kaylaaltepeter.com>
2020-07-20 08:42:10 -0500

    feat(ng-demo/feature-shell/shell): creating
    `nx generate @schematics/angular:component --name=shell
    --project=ng-demo-feature-shell --style=scss --entryComponent --prefix=ngd`

--- libs/ng-demo/feature-shell/src/lib/ng-demo-feature-shell.module.ts
+++ libs/ng-demo/feature-shell/src/lib/ng-demo-feature-shell.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ShellComponent } from './shell/shell.component';
 
 @NgModule({
   imports: [
@@ -10,5 +11,7 @@ import { RouterModule } from '@angular/router';
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
   ],
+  declarations: [ShellComponent],
+  entryComponents: [ShellComponent],
 })
 export class NgDemoFeatureShellModule {}
